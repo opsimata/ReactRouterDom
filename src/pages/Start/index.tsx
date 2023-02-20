f7728a142f2924ff99b6a071c309934795defd9b
Pedro <pedro.enrique1599@icloud.com>
2023-02-20 02:26:51 -0300

    eslint configs, routing and refactoring start

---
+++ src/pages/Start/index.tsx
@@ -0,0 +1,10 @@
+import menu from "pages/Menu/Items/items.json";
+
+export default function Start() {
+	return (
+		<section>
+			<h3>Recomendações da cozinha</h3>
+			<div></div>
+		</section>
+	);
+}
