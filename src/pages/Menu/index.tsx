a7e903a42de3ca2ec1b93c9d63dfe8bf9b7b93a8
Pedro <pedro.enrique1599@icloud.com>
2023-02-22 04:23:19 -0300

    header component refactored to the router

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -4,7 +4,6 @@ import { useState } from "react";
 import Filters from "./Filters";
 import Sorter from "./Sorter";
 import Items from "./Items";
-import Navbar from "components/Navbar";
 
 export default function Menu() {
 	const [find, setFinder] = useState("");
@@ -12,19 +11,14 @@ export default function Menu() {
 	const [sorter, setSorter] = useState("");
 
 	return (
-		<main>
-			<header className={styles.header}>
-				<div className={styles.header__text}>A casa do código e da massa</div>
-			</header>
-			<section className={styles.carte}>
-				<h3 className={styles.carte__title}>Cardápio</h3>
-				<Finder find={find} setFinder={setFinder} />
-				<div className={styles.carte__filters}>
-					<Filters filter={filter} setFilter={setFilter} />
-					<Sorter sorter={sorter} setSorter={setSorter} />
-				</div>
-				<Items find={find} filter={filter} sorter={sorter}/>
-			</section>
-		</main>
+		<section className={styles.carte}>
+			<h3 className={styles.carte__title}>Cardápio</h3>
+			<Finder find={find} setFinder={setFinder} />
+			<div className={styles.carte__filters}>
+				<Filters filter={filter} setFilter={setFilter} />
+				<Sorter sorter={sorter} setSorter={setSorter} />
+			</div>
+			<Items find={find} filter={filter} sorter={sorter}/>
+		</section>
 	);
 }
--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,21 @@ import Navbar from "components/Navbar";
 import Menu from "pages/Menu";
 import Start from "pages/Start";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styles from "./Routes.module.scss";
 
 export default function AppRouter() {
 	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Start />} />
-				<Route path="/menu" element={<Menu />} />
-			</Routes>
-		</Router>
+		<main>
+			<Router>
+				<Navbar />
+				<header className={styles.header}>
+					<div className={styles.header__text}>A casa do código e da massa</div>
+				</header>
+				<Routes>
+					<Route path="/" element={<Start />} />
+					<Route path="/menu" element={<Menu />} />
+				</Routes>
+			</Router>
+		</main>
 	);
 }
